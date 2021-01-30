06e3027722254cb4a384147a4bf2b716518fbf70
derek <derekle.creative@gmail.com>
2021-01-30 02:53:49 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")? true : false
   end
 
   def question?
-
+    self.end_with?("?")? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!")? true : false
   end
 
   def count_sentences
-
+    #x = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+    self.split(/[.?!]+/).grep(/\S/).count
   end
 end
